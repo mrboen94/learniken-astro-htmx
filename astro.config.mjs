fa3076d3b085f16ac43db25a4b5cae78aef8858d
mrboen94 <mrboen94@gmail.com>
2024-02-03 01:36:08 +0100

    Just messing about with view transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
+
+import htmx from "astro-htmx";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind()]
+  integrations: [tailwind(), htmx()],
+  adapter: node({
+    mode: "standalone"
+  })
 });
